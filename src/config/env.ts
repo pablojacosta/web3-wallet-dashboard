4a05f6af8f81fc1a05eb6e74373ea9aed67f8574
pablojacosta <pablojacosta82@gmail.com>
2025-03-31 19:36:32 -0300

    fix: build. remove jest config file. remove unused env variable in env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,6 @@ import { Env } from '~/types';
 
 const env: Env = {
   PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID as string,
-  TEST_ENV: (process.env.NEXT_PUBLIC_TEST_ENV as string) === 'true',
 };
 
 export const getEnv = (): Env => {
